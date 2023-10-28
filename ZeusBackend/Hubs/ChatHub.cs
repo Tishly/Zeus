985ec72cdece8ffb4996ec6ac3ea5ba3ed2e7877
fearless-ajs <b-jadurotimi@microsoft.com>
2023-10-28 15:40:03 +0100

    initial commit

---
+++ ZeusBackend/Hubs/ChatHub.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ZeusBackend.Hubs
+{
+    public class ChatHub : Hub
+    {
+
+        // This method will send notifications to all clients
+        // If client have to communicate, it will call <SendMessage()> method
+        // If client have to receive notification from server server it will use.
+        // <receiveMessage> method.
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public async Task JoinChat(string user, string message)
+        {
+            await Clients.Others.SendAsync("ReceiveMessage", user, message);
+        }
+        
+
+
+        
+    }
+}
