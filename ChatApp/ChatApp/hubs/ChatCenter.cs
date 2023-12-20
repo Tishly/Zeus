546fc78319aca9dd2fccc0b56cb0db44e843a544
Jerry Abadi <abadijerry44@gmail.com>
2023-12-20 11:03:19 +0100

    new changes added
    new changes made:
    1. ChatApp(containing c# files)
    2. Web-Chat Client (containing js, html, css files)

---
+++ ChatApp/ChatApp/hubs/ChatCenter.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChatApp.hubs
+{
+    public class ChatCenter : Hub 
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public async Task JoinChat(string user, string message)
+        {
+            await Clients.Others.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
